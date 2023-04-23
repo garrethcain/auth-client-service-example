0556343b91fc193dedc74d0f43da5586922f9ef4
Garreth <garrethc@credfin.io>
2023-04-24 07:55:36 +1000

    fix: used incorrect urls.py causing a loop

--- auth-service/config/settings.py
+++ auth-service/config/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     "rest_framework_simplejwt",
-    "remotejwt",
     'remotejwt_user',
 ]
 
--- auth-service/config/urls.py
+++ auth-service/config/urls.py
@@ -16,14 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('auth/',
-         include(
-            [
-                path('', include('remotejwt.urls')),
-                path('', include('remotejwt_user.urls')),
-            ]
-        )
-    )
+    path("auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("auth/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    # path("auth/users/<int:pk>/", TokenUserDetailView.as_view(), name="user_detail"),
+    path('', include('remotejwt_user.urls')),
 ]
